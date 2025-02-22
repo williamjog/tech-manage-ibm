6ec9d439e57dfcb321bc175e17c37fa0bdb045d7
William <william.gomes@enacom.com.br>
2025-02-22 18:15:11 -0300

    fix: remove aria-hidden do app-root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { UserFormComponent } from './components/user-form/user-form.component';
 
@@ -13,11 +13,13 @@ export class AppComponent {
   currentYear: number;
 
   constructor(
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private renderer: Renderer2
   ) {}
 
   ngOnInit(): void {
     this.currentYear = new Date().getFullYear();
+    this.renderer.removeAttribute(document.querySelector('app-root'), 'aria-hidden');
   }
 
   openUserModal(): void {
